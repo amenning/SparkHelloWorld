91a28d8a8cf07922f7ecac79532ca328257f03ac
metakata <menning23@gmail.com>
2016-04-11 21:34:17 -0400

    example spark hello world example

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> 
+        		"Hello World<br/>" +
+        		"Hello World"
+        );
+    }
+}
